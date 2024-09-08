90d553181051626c6c9360591d5a35dddbca8cea
Big Phil <deitrix@users.noreply.github.com>
2024-09-08 12:33:30 +0100

    Use sqlg

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/deitrix/sqlg v0.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
--- store/mysql/store.go
+++ store/mysql/store.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 
 	"github.com/deitrix/gqlgen-dataloadgen-example/graph/model"
-	"github.com/deitrix/gqlgen-dataloadgen-example/store/mysql/db"
+	"github.com/deitrix/sqlg"
 	"github.com/doug-martin/goqu/v9"
 
 	_ "github.com/doug-martin/goqu/v9/dialect/mysql"
@@ -24,36 +24,38 @@ var (
 	postsTable = goqu.T("posts")
 )
 
+var mySQL = goqu.Dialect("mysql")
+
 func (s Store) CreateUser(ctx context.Context, user model.User) (int, error) {
-	return db.Insert(ctx, s.db, db.MySQL.
+	return sqlg.ExecID(ctx, s.db, mySQL.
 		Insert(usersTable).
 		Rows(goqu.Record{"name": user.Name}))
 }
 
 func (s Store) UpdateUser(ctx context.Context, id int, user model.User) error {
-	return db.Exec(ctx, s.db, db.MySQL.
+	return sqlg.Exec(ctx, s.db, mySQL.
 		Update(usersTable).
 		Set(goqu.Record{"name": user.Name}).
 		Where(usersTable.Col("id").Eq(id)))
 }
 
 func (s Store) DeleteUser(ctx context.Context, id int) error {
-	return db.Exec(ctx, s.db, db.MySQL.
+	return sqlg.Exec(ctx, s.db, mySQL.
 		Delete(usersTable).
 		Where(usersTable.Col("id").Eq(id)))
 }
 
 func (s Store) Users(ctx context.Context) ([]model.User, error) {
-	return db.SelectAll(ctx, s.db, scanUser, usersQuery())
+	return sqlg.SelectAll(ctx, s.db, scanUser, usersQuery())
 }
 
 func (s Store) UserByID(ctx context.Context, id int) (model.User, error) {
-	return db.Select(ctx, s.db, scanUser, usersQuery().
+	return sqlg.Select(ctx, s.db, scanUser, usersQuery().
 		Where(usersTable.Col("id").Eq(id)))
 }
 
 func (s Store) UsersByIDs(ctx context.Context, ids []int) (map[int]model.User, error) {
-	users, err := db.SelectAll(ctx, s.db, scanUser, usersQuery().
+	users, err := sqlg.SelectAll(ctx, s.db, scanUser, usersQuery().
 		Where(usersTable.Col("id").In(ids)))
 	if err != nil {
 		return nil, err
@@ -66,35 +68,35 @@ func (s Store) UsersByIDs(ctx context.Context, ids []int) (map[int]model.User, e
 }
 
 func (s Store) CreatePost(ctx context.Context, userID int, post model.Post) (int, error) {
-	return db.Insert(ctx, s.db, db.MySQL.
+	return sqlg.ExecID(ctx, s.db, mySQL.
 		Insert(postsTable).
 		Rows(goqu.Record{"title": post.Title, "text": post.Text, "user_id": userID}))
 }
 
 func (s Store) UpdatePost(ctx context.Context, id int, post model.Post) error {
-	return db.Exec(ctx, s.db, db.MySQL.
+	return sqlg.Exec(ctx, s.db, mySQL.
 		Update(postsTable).
 		Set(goqu.Record{"title": post.Title, "text": post.Text}).
 		Where(postsTable.Col("id").Eq(id)))
 }
 
 func (s Store) DeletePost(ctx context.Context, id int) error {
-	return db.Exec(ctx, s.db, db.MySQL.
+	return sqlg.Exec(ctx, s.db, mySQL.
 		Delete(postsTable).
 		Where(postsTable.Col("id").Eq(id)))
 }
 
 func (s Store) Posts(ctx context.Context) ([]model.Post, error) {
-	return db.SelectAll(ctx, s.db, scanPost, postsQuery())
+	return sqlg.SelectAll(ctx, s.db, scanPost, postsQuery())
 }
 
 func (s Store) PostByID(ctx context.Context, id int) (model.Post, error) {
-	return db.Select(ctx, s.db, scanPost, postsQuery().
+	return sqlg.Select(ctx, s.db, scanPost, postsQuery().
 		Where(postsTable.Col("id").Eq(id)))
 }
 
 func (s Store) PostsByIDs(ctx context.Context, ids []int) (map[int]model.Post, error) {
-	posts, err := db.SelectAll(ctx, s.db, scanPost, postsQuery().
+	posts, err := sqlg.SelectAll(ctx, s.db, scanPost, postsQuery().
 		Where(postsTable.Col("id").In(ids)))
 	if err != nil {
 		return nil, err
@@ -107,12 +109,12 @@ func (s Store) PostsByIDs(ctx context.Context, ids []int) (map[int]model.Post, e
 }
 
 func (s Store) PostsByUserID(ctx context.Context, userID int) ([]model.Post, error) {
-	return db.SelectAll(ctx, s.db, scanPost, postsQuery().
+	return sqlg.SelectAll(ctx, s.db, scanPost, postsQuery().
 		Where(postsTable.Col("user_id").Eq(userID)))
 }
 
 func (s Store) PostsByUserIDs(ctx context.Context, userIDs []int) (map[int][]model.Post, error) {
-	posts, err := db.SelectAll(ctx, s.db, scanPost, postsQuery().
+	posts, err := sqlg.SelectAll(ctx, s.db, scanPost, postsQuery().
 		Where(postsTable.Col("user_id").In(userIDs)))
 	if err != nil {
 		return nil, err
@@ -125,25 +127,21 @@ func (s Store) PostsByUserIDs(ctx context.Context, userIDs []int) (map[int][]mod
 }
 
 func usersQuery() *goqu.SelectDataset {
-	return db.MySQL.
+	return mySQL.
 		Select("id", "name").
 		From("users")
 }
 
-func scanUser(row db.Row) (model.User, error) {
-	var user model.User
-	err := row.Scan(&user.ID, &user.Name)
-	return user, err
+func scanUser(row sqlg.Row) (user model.User, err error) {
+	return user, row.Scan(&user.ID, &user.Name)
 }
 
 func postsQuery() *goqu.SelectDataset {
-	return db.MySQL.
+	return mySQL.
 		Select("id", "title", "text", "user_id").
 		From("posts")
 }
 
-func scanPost(row db.Row) (model.Post, error) {
-	var post model.Post
-	err := row.Scan(&post.ID, &post.Title, &post.Text, &post.UserID)
-	return post, err
+func scanPost(row sqlg.Row) (post model.Post, err error) {
+	return post, row.Scan(&post.ID, &post.Title, &post.Text, &post.UserID)
 }
